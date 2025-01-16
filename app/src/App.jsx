316b4dd5a449a07bfcd4a515ef286b0b2773d7f8
William Bradshaw <w.a.bradshaw@gmail.com>
2025-01-16 17:47:39 +0000

    Restyled with new scheme

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -7,7 +7,9 @@ import themes from './cards/baseThemes.json';
 function App() {
   return (
     <div className="App">
-      <h1>Gridpinions</h1>
+      <div className="name">
+        <img src="name.png"/>
+      </div>
       <Board scale={3} opinionsList={opinions} descriptionsList={descriptions} themesList={themes}/>      
     </div>
   );
